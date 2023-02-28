d74c1f35c78586aa04d0ee0ccfbc6dcf9d20ca9b
Rixen <rixennoct@rambler.ru>
2023-03-01 00:58:07 +0300

    Registration

--- app/src/main/java/com/example/socialnetworksjs/view/LoginFragment.kt
+++ app/src/main/java/com/example/socialnetworksjs/view/LoginFragment.kt
@@ -1,10 +1,13 @@
 package com.example.socialnetworksjs.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.socialnetworksjs.R
 import com.example.socialnetworksjs.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -21,6 +24,18 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val registerButton = binding.bRegister
+        registerButton.setOnClickListener {
+            startActivity(getIntentForRegister())
+        }
+    }
+
+    private fun getIntentForRegister(): Intent {
+        val url = getString(R.string.register_url)
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        return intent
     }
 
     companion object {
