82f4f9343ece9fc3c4d412596899aedafbf1910e
Rixen <rixennoct@rambler.ru>
2023-03-03 03:51:33 +0300

    LiveData encapsulated

--- app/src/main/java/com/example/socialnetworksjs/view/LoginViewModel.kt
+++ app/src/main/java/com/example/socialnetworksjs/view/LoginViewModel.kt
@@ -2,18 +2,23 @@ package com.example.socialnetworksjs.view
 
 import android.content.Intent
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel() {
 
-    val intentForRegister = MutableLiveData<Intent>()
+    private val intentForRegister = MutableLiveData<Intent>()
 
-    fun getIntentForRegister() {
+    fun register() {
         val url = REGISTER_PAGE_URL
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         intentForRegister.value = intent
+    }
+
+    fun getIntentForRegister(): LiveData<Intent>{
+        return intentForRegister
     }
 
 //    fun logIn(login: String, password: String) {
